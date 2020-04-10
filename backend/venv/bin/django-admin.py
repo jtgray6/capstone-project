b2bb41adcdb829ba32b9c23b771dcc0b842f6c8b
Jeremy Gray <jeremytgray6@gmail.com>
2020-04-09 22:20:40 -0500

    setup inital django backend with jwt auth and vue frontend

---
+++ backend/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jeremygray/Flatiron School/Mod5/project/backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
